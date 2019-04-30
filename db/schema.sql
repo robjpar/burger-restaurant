78bbd56c3adda81828005f901b6a178477f1c009
Robert J. Paruch <roobbb@live.com>
2019-04-30 00:57:57 -0700

    Added the database structure and initial data

---
+++ db/schema.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE IF NOT EXISTS burger_db;
+
+USE burger_db;
+
+CREATE TABLE IF NOT EXISTS burgers (
+    burger_id INT NOT NULL AUTO_INCREMENT,
+    burger_name VARCHAR(100) NOT NULL,
+    delivered BOOLEAN DEFAULT FALSE,
+    ordered_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    delivered_at DATETIME ON UPDATE CURRENT_TIMESTAMP,
+    PRIMARY KEY (burger_id)
+);
---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+USE burger_db;
+
+INSERT INTO burgers
+(burger_name)
+VALUES
+('Hamburger'),
+('Cheeseburger'),
+('Bacon Smokehouse Burger');
